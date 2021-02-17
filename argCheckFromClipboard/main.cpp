c607a1ce5e3aeb9801af64182b3c3440dab5b993
ambiesoft <ambiesoft.trueff@gmail.com>
2021-02-17 20:26:41 +0900

    I18N

--- argCheckFromClipboard/main.cpp
+++ argCheckFromClipboard/main.cpp
@@ -6,6 +6,7 @@
 #include "../../lsMisc/GetClipboardText.h"
 #include "../../lsMisc/OpenCommon.h"
 #include "../../lsMisc/stdosd/stdosd.h"
+#include "../../lsMisc/I18N.h"
 
 using namespace Ambiesoft;
 using namespace Ambiesoft::stdosd;
@@ -13,15 +14,27 @@ using namespace std;
 
 #define APPNAME L"argCheckFromClipboard"
 
+#define KAIGYO L"\r\n";
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
 	_In_ int       nCmdShow)
 {
+	i18nInitLangmap(NULL, NULL, APPNAME);
 	wstring text;
 	if (!GetClipboardText(nullptr, text))
 	{
-		MessageBox(nullptr, GetLastErrorString(GetLastError()).c_str(), APPNAME, MB_ICONHAND);
+		DWORD dwLE = GetLastError();
+		wstring message;
+		message += I18N(L"Failed to get text from clipboard.");
+		if (dwLE != NO_ERROR)
+		{
+			message += KAIGYO;
+			message += KAIGYO;
+			message += GetLastErrorString(dwLE);
+		}
+		MessageBox(nullptr, message.c_str(), APPNAME, MB_ICONHAND);
 		return 1;
 	}
 
